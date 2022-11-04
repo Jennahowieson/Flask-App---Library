b6ff0a5c1be0c2d9e5f02570fffb0ce8709cf2a7
Jenna Howieson <jenna@jennahowieson.co.uk>
2022-11-04 18:51:27 +0000

    main list displays correctly

--- controllers/controller.py
+++ controllers/controller.py
@@ -7,3 +7,15 @@ from flask import render_template, request
 @app.route ('/book_list')
 def book_list():
     return render_template('book_list.html', book_list = library.book_list)
+
+@app.route ('/book_list/<book_title>')
+def single_book_page(book_title):
+    for book in library.book_list:
+        if book_title in library.book_list:
+            return render_template('single_book.html', book = book_list.index(book.title))
+
+# @app.route ('/book_list/<book_num>')
+# def single_book_page(book_num):
+#     return render_template('single_book.html', book=library.book_list[int(book_num)])
+
+# book_title_index = book_list.index(book.title)
